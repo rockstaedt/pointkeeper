872e9721dff0c62de331e6a2947ae2f81faade11
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2020-12-26 13:44:39 +0100

    Adapt start screen

--- doppelkopf/endpoints/ui/ui_basis.py
+++ doppelkopf/endpoints/ui/ui_basis.py
@@ -7,6 +7,8 @@ from flask import (
     redirect
 )
 from datetime import datetime
+import pandas as pd
+from sqlalchemy import select
 
 from doppelkopf.extensions import db
 
@@ -19,36 +21,44 @@ from doppelkopf.resource_models import (
 
 ui_basis = Blueprint('ui_basis', __name__)
 
-@ui_basis.route('/home', methods = ['GET'])
-# @basis.route('/#spielliste', methods = ['GET'])
-# @basis.route('/#rangliste', methods = ['GET'])
-def home():
-    games = Game.query.order_by(Game.date).all()
-    players = Player.query.order_by(Player.name).all()
-    player_results = {}
-    for game in games:
-        player_results[game.id] = result_rm.get_player_results(
-            game_id = game.id
-        )
-    player_statistics = {}
-    for player in players:
-        # update here again because one player is always missing
-        # in game update / save / delete
-        player_rm.update_game_statistics(player.id)
-        db.session.commit()
-        player_statistics[player.id] = player_rm.get_game_statistic_player(
-            player.id
-        )
-    players_ranked = Player.query.order_by(Player.ranking).all()
-    return render_template('home.html')
-    # return render_template('index.html',
-    #     games = games,
-    #     players = players,
-    #     players_ranked  = players_ranked,
-    #     player_results = player_results,
-    #     player_statistics =  player_statistics
-    # )
+# @ui_basis.route('/home', methods = ['GET'])
+# # @basis.route('/#spielliste', methods = ['GET'])
+# # @basis.route('/#rangliste', methods = ['GET'])
+# def home():
+#     games = Game.query.order_by(Game.date).all()
+#     players = Player.query.order_by(Player.name).all()
+#     player_results = {}
+#     for game in games:
+#         player_results[game.id] = result_rm.get_player_results(
+#             game_id = game.id
+#         )
+#     player_statistics = {}
+#     for player in players:
+#         # update here again because one player is always missing
+#         # in game update / save / delete
+#         player_rm.update_game_statistics(player.id)
+#         db.session.commit()
+#         player_statistics[player.id] = player_rm.get_game_statistic_player(
+#             player.id
+#         )
+#     players_ranked = Player.query.order_by(Player.ranking).all()
+#     # return render_template('index.html',
+#     #     games = games,
+#     #     players = players,
+#     #     players_ranked  = players_ranked,
+#     #     player_results = player_results,
+#     #     player_statistics =  player_statistics
+#     # )
 
-@ui_basis.route('/', methods = ['GET'])
+@ui_basis.route('/', methods=['GET'])
 def start():
-    return render_template('index.html')
+    return render_template('start.html')
+
+@ui_basis.route('/home', methods=['GET'])
+def home():
+    games= Game.query.order_by(Game.date).all()
+    games_count = len(games)
+    return render_template(
+        'home.html',
+        games_count = games_count
+    )
