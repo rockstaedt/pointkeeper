94fa48f218fdc9991b39f9a40a8841e83d700dd5
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-01-02 17:20:04 +0100

    Add functions for total games, batches, placements

--- doppelkopf/resource_models/result_rm.py
+++ doppelkopf/resource_models/result_rm.py
@@ -1,4 +1,5 @@
 from sqlalchemy import func, desc
+from typing import Dict
 
 from doppelkopf.extensions import db
 
@@ -53,3 +54,56 @@ def get_total_points_player(player_id: int) -> int:
         return results.total_points_sum
     else:
         return 0
+
+def get_total_batches_all() -> Dict:
+    results = db.session.query(
+        Result.player_id,
+        func.sum(Game.played_matches).label("total_games_sum")
+    ).join(
+        Game
+    ).group_by(
+        Result.player_id
+    ).all()
+    player_id_to_total_batches = {}
+    for result in results:
+        player_id_to_total_batches[result.player_id] = result.total_games_sum
+    return player_id_to_total_batches
+
+def get_total_games_all() -> Dict:
+    players = Player.query.all()
+    player_id_to_total_games = {
+        player.id: 0 for player in players
+    }
+    for player in players:
+        played_games =db.session.query(
+            Result.player_id
+        ).filter(
+            Result.player_id == player.id
+        ).all()
+        for played_game in played_games:
+            player_id_to_total_games[player.id] += 1
+    return player_id_to_total_games
+
+def get_counter_placement_all(placement:int) -> Dict:
+    players = Player.query.all()
+    # init dictionary to store
+    player_id_to_placement_counter = {player.id: 0 for player in players}
+    games = Game.query.all()
+    for game in games:
+        results = Result.query.filter(
+            Result.game_id == game.id
+        ).order_by(
+            desc(Result.points)
+        ).all()
+        if placement <= len(results):
+            if placement != -1:
+                index = placement - 1
+            else:
+                index = placement
+            player_id_to_placement_counter[results[index].player_id] += 1
+        else:
+            raise ValueError(
+                f'Placement {placement} can not be determined for '
+                + f'{len(results)} results for game {game.id}.'
+            )
+    return player_id_to_placement_counter
