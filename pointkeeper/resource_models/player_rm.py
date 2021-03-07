059eb0f5313a24f5b682e8f80ccb41f87181ab05
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-03-07 11:56:46 +0100

    Fix issue #3

--- pointkeeper/resource_models/player_rm.py
+++ pointkeeper/resource_models/player_rm.py
@@ -40,6 +40,9 @@ def update_game_statistics(player_id):
 
 
 def get_statistics_players() -> OrderedDict:
+    players = Player.query.all()
+    for player in players:
+        update_game_statistics(player.id)
     results = db.session.query(Player).order_by(Player.ranking).all()
     result_dic = OrderedDict()
     for i, result in enumerate(results):
