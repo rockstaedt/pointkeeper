3a0e56e394c58e276c0ccf8c4e7b921d260dd841
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-04-30 12:10:48 +0200

    Add option to specify the order

--- pointkeeper/resource_models/game_rm.py
+++ pointkeeper/resource_models/game_rm.py
@@ -5,14 +5,19 @@ from pointkeeper.extensions import db
 from pointkeeper.models import Game
 
 
-def get_list_played_years() -> list:
+def get_list_played_years(desc: bool = True) -> list:
     """
-    This function returns a list of years in which games happened in ascending
-    order.
-    :return: List of years in ascending order.
+    This function returns a list of years in which games happened in descending
+    order if not specified otherwise.
+
+    :param desc: Specifies the ordering of the list.
+    :return: List of years.
     """
     result = db.session.query(
         func.min(extract('year', Game.date)).label('year_min'),
         func.max(extract('year', Game.date)).label('year_max')
     ).one()
-    return list(range(int(result.year_max), int(result.year_min)-1, -1))
+    if desc:
+        return list(range(int(result.year_max), int(result.year_min)-1, -1))
+    else:
+        return list(range(int(result.year_min), int(result.year_max) + 1))
